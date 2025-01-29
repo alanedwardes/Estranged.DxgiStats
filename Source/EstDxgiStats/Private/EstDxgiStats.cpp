ffab12a166ed597e2283a1697fb43197ad52dd22
Alan Edwardes <alan@alanedwardes.com>
2025-01-29 22:24:39 +0000

    Further improvements to initialization.

--- Source/EstDxgiStats/Private/EstDxgiStats.cpp
+++ Source/EstDxgiStats/Private/EstDxgiStats.cpp
@@ -6,25 +6,6 @@ IMPLEMENT_GAME_MODULE(FEstDxgiStatsModule, EstDxgiStats);
 
 void FEstDxgiStatsModule::StartupModule()
 {
-	CreateDXGIFactory1(__uuidof(IDXGIFactory4), (void**)&DxgiFactory);
-
-    ID3D12Device* NativeDevice = static_cast<ID3D12Device*>(GDynamicRHI->RHIGetNativeDevice());
-    LUID AdapterLuid = NativeDevice->GetAdapterLuid();
-
-    int32 i = 0;
-    IDXGIAdapter3* adapter;
-    DXGI_ADAPTER_DESC adapterDescription;
-    while (DxgiFactory->EnumAdapters(i, reinterpret_cast<IDXGIAdapter**>(&adapter)) != DXGI_ERROR_NOT_FOUND)
-    {
-        i++;
-        adapter->GetDesc(&adapterDescription);
-        if (adapterDescription.AdapterLuid.HighPart == AdapterLuid.HighPart &&
-            adapterDescription.AdapterLuid.LowPart == AdapterLuid.LowPart)
-        {
-            ActiveGraphicsAdapter = adapter;
-            break;
-        }
-    }
 }
 
 void FEstDxgiStatsModule::ShutdownModule()
@@ -38,4 +19,35 @@ void FEstDxgiStatsModule::ShutdownModule()
     {
         DxgiFactory->Release();
     }
+}
+
+IDXGIAdapter3* FEstDxgiStatsModule::GetActiveGraphicsAdapter()
+{
+    if (DxgiFactory == nullptr)
+    {
+        CreateDXGIFactory1(__uuidof(IDXGIFactory4), (void**)&DxgiFactory);
+    }
+
+    if (ActiveGraphicsAdapter == nullptr)
+    {
+        ID3D12Device* NativeDevice = static_cast<ID3D12Device*>(GDynamicRHI->RHIGetNativeDevice());
+        LUID AdapterLuid = NativeDevice->GetAdapterLuid();
+
+        int32 i = 0;
+        IDXGIAdapter3* adapter;
+        DXGI_ADAPTER_DESC adapterDescription;
+        while (DxgiFactory->EnumAdapters(i, reinterpret_cast<IDXGIAdapter**>(&adapter)) != DXGI_ERROR_NOT_FOUND)
+        {
+            i++;
+            adapter->GetDesc(&adapterDescription);
+            if (adapterDescription.AdapterLuid.HighPart == AdapterLuid.HighPart &&
+                adapterDescription.AdapterLuid.LowPart == AdapterLuid.LowPart)
+            {
+                ActiveGraphicsAdapter = adapter;
+                break;
+            }
+        }
+    }
+
+    return ActiveGraphicsAdapter;
 }
--- Source/EstDxgiStats/Public/EstDxgiStats.h
+++ Source/EstDxgiStats/Public/EstDxgiStats.h
@@ -7,7 +7,7 @@ class ESTDXGISTATS_API FEstDxgiStatsModule : public IModuleInterface
 public:
 	virtual void StartupModule();
 	virtual void ShutdownModule();
-	static class IDXGIAdapter3* GetActiveGraphicsAdapter() { return ActiveGraphicsAdapter; }
+	static class IDXGIAdapter3* GetActiveGraphicsAdapter();
 private:
 	inline static class IDXGIAdapter3* ActiveGraphicsAdapter;
 	inline static class IDXGIFactory4* DxgiFactory;
